83d52a1dca8744d1ed69f1c35bd9fbb6473848cf
rashadazimov <rashadazimov23@gmail.com>
2022-05-21 12:07:26 +0400

    blue bank application

--- src/main/java/com/bluebank/BlueBankApplication.java
+++ src/main/java/com/bluebank/BlueBankApplication.java
@@ -3,13 +3,20 @@ package com.bluebank;
 import com.bluebank.model.*;
 import com.bluebank.repository.AccountRepository;
 import com.bluebank.repository.CustomerRepository;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableCaching
+
 public class BlueBankApplication implements CommandLineRunner {
 
 	private final AccountRepository accountRepository;
@@ -25,13 +32,23 @@ public class BlueBankApplication implements CommandLineRunner {
 		SpringApplication.run(BlueBankApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+								 @Value("${application-version}") String version){
+		return new OpenAPI()
+				.info(new Info()
+						.title("BlueBank API")
+						.version(version)
+						.description(description)
+						.license(new License().name("BlueBank API Licence")));
+	}
 
 	@Override
 	public void run(String... args) throws Exception{
 		Customer c1 = Customer.builder()
 				.id("1234568")
 				.name("Rahim")
-				.address(Address.builder().city(City.Baku).postcode("456312").addressDetails("bu bir adrestir").build())
+				.address(Address.builder().city(City.Baku).postcode("456312").addressDetails("this is address").build())
 				.city(City.Baku)
 				.dateOfBirth(1988)
 				.build();
@@ -41,7 +58,7 @@ public class BlueBankApplication implements CommandLineRunner {
 				.id("789456")
 				.name("Rashad")
 				.city(City.Lankaran)
-				.address(Address.builder().city(City.Lankaran).postcode("456312").addressDetails("bu bir adrestir 2").build())
+				.address(Address.builder().city(City.Lankaran).postcode("456312").addressDetails("this is address 2").build())
 				.dateOfBirth(2000)
 				.build();
 
@@ -49,7 +66,7 @@ public class BlueBankApplication implements CommandLineRunner {
 				.id("456238")
 				.name("Fuad")
 				.city(City.Sumgait)
-				.address(Address.builder().city(City.Sumgait).postcode("456312").addressDetails("bu bir adrestir 3").build())
+				.address(Address.builder().city(City.Sumgait).postcode("456312").addressDetails("this is address 3").build())
 				.dateOfBirth(2005)
 				.build();
 
